f4e533b60e72322fd43a2f1b200631ddbba8129e
Ryan Breen <ryanbreen85@gmail.com>
2014-04-16 19:54:45 -0400

    Prettified code a bit

--- customwatch.sh
+++ customwatch.sh
@@ -1,9 +1,18 @@
-echo "How many folders, relative to this location, do you want to monitor?" # Figure out how many folders the user wants to check.
-read number # Assign that number to variable $number
-i=1 # Begin incrementation for while loop
+#!/usr/bin/env bash
+
+# Figure out how many folders the user wants to check.
+echo "How many folders, relative to this location, do you want to monitor?" 
+
+# Assign that number to variable $number
+read number 
+
+# Begin incrementation for while loop
+i=1 
 folders=() # Get a nice clean array
 
-## Below, we are opening a while loop, starting with the number 1, counting up, and asking the user to tell which directories to check, then adding them to directory array $folders
+# Below, we are opening a while loop, starting with the number 1, counting up,
+# and asking the user to tell which directories to check, then adding them to
+# directory array $folders.
 while [ "$i" -le "$number" ]; do	
 	echo -n "Folder "; echo -n $i; echo -n " is:"
 	read name
@@ -11,14 +20,11 @@ while [ "$i" -le "$number" ]; do
 	i=$((i+1))
 	done
 	
-# echo ${folders[@]} # A test to see if the above loop worked.
-
 commandarray=()
 for n in ${folders[@]}; do
-	command="echo -n 'FOLDER'; echo $n && ls -lsah $n | tail -n5 &&"
+	command="echo -n 'FOLDER'; echo $n && ls -lsahrt $n | tail -n5 &&"
 	commandarray+=($command)
 	done
 
 lsstring=`echo ${commandarray[@]} | sed -e "s/&&$//"`
-# echo $lsstring # Another test
 watch -n 1 "$lsstring"
